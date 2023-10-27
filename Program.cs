c455d3c7f574f78a996b883ac18de7b020d18854
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-10-27 14:49:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public string Name { get; set; }
+    public int StudentID { get; set; }
+
+    public Student(string name, int studentID)
+    {
+        Name = name;
+        StudentID = studentID;
+    }
+}
+
+class StudentGroup
+{
+    public string GroupName { get; set; }
+    public List<Student> Students { get; }
+
+    public StudentGroup(string groupName)
+    {
+        GroupName = groupName;
+        Students = new List<Student>();
+    }
+
+    
+    public void AddStudent(Student student)
+    {
+        Students.Add(student);
+    }
+
+    
+    public void RemoveStudent(Student student)
+    {
+        Students.Remove(student);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        StudentGroup group = new StudentGroup("Группа A");
+        group.AddStudent(new Student("Микрочелик", 1));
+        group.AddStudent(new Student("Кровавая Мэри", 2));
+        group.AddStudent(new Student("Я", 3));
+
+        
+        Console.WriteLine("Студенты в группе " + group.GroupName + ":");
+        foreach (var student in group.Students)
+        {
+            Console.WriteLine("Имя: " + student.Name + ", № студента: " + student.StudentID);
+        }
+    }
+}
